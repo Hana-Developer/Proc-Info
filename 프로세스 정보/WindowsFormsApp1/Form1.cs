ccc5b77b72d6c48ef38739c93742e20ad93e0316
Hana-Developer <74347091+Hana-Developer@users.noreply.github.com>
2021-02-15 15:03:04 +0900

    Add files via upload

---
+++ 프로세스 정보/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process[] allProc = Process.GetProcesses();
+
+            foreach(Process proc in allProc)
+            {
+                listBox1.Items.Add("====================================");
+                listBox1.Items.Add(String.Format("프로세스 이름 : {0}", proc.ProcessName));
+                listBox1.Items.Add(String.Format("프로세스 ID : {0}", proc.Id));
+                listBox1.Items.Add("====================================");
+            }
+        }
+    }
+}
